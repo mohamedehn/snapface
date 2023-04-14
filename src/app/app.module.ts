e14bebfd5567947a40cff3d540876bc98d285e40
Mohamed <mohamed.ebarhmatin@yahoo.fr>
2023-04-15 00:58:18 +0200

    first component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { FaceSnapComponent } from './face-snap/face-snap.component';
+import { FaceSnapComponent } from "./face-snap/face-snap.component";
 
 @NgModule({
   declarations: [
