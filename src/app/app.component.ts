b51915a2b975dbdf472ad8ea5fa0fb0b13820386
Mohamed <mohamed.ebarhmatin@yahoo.fr>
2023-05-04 21:54:17 +0200

    Testing Observable on app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+
+  //on déclare le type de  interval$  , on le déclare comme  Observable  qui émet des  number  en passant  number  entre chevrons <>
+  interval$! : Observable<number> 
   
   ngOnInit(){
-    
+    this.interval$ = interval(1000);
   }
 }
